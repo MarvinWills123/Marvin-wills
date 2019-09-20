ebb2977c18e07f4616ee63fc208640c37f6d7e7c
Marvin <Player1@Marvins-World.local>
2019-09-19 21:17:10 -0500

    Add greeter comment.

---
+++ scripts.js
@@ -0,0 +1,12 @@
+function greeter(question = "What's your name?"){
+  return prompt(question);
+}
+
+const answer = greeter("What's your favorite scary movie?");
+console.log(answer);
+
+
+document.querySelector('h1').textContent = greeter();
+//const h1 = document.querySelector("h1");
+//const textContent = h1.textContent
+
