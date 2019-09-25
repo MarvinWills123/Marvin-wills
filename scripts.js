935f9b0663a2c2267d25cbd160cb6e3736308326
Marvin <Player1@Marvins-World.local>
2019-09-24 21:21:03 -0500

    Add Hamburger functionality.

--- scripts.js
+++ scripts.js
@@ -9,12 +9,12 @@ if (answer === ""){
 return answer;
 }
 
-document.querySelector('h1').textContent = greeter();{
+//document.querySelector('h1').textContent = greeter();
 //const h1 = document.querySelector("h1");
 //const textContent = h1.textContent
 
-document.querySelector("#root").innerHTML="<p>" + greeter() + "</p>";
-document.querySelector("#root").innerHTML = `<p>${greeter()}</p>`
+//document.querySelector("#root").innerHTML="<p>" + greeter() + "</p>";
+document.querySelector("#root").innerHTML = `<p>${greeter()}</p>`;
 
 
 
@@ -31,4 +31,18 @@ const getter = me.name;
 console.log(getter);
 
 //Setter
-me.name= "Marvin"
+me.name= "Marvin";
+
+//select fa-hamburger
+//listen for click on the hamburger
+//Remove the 'mobile hidden' class from the something
+
+const hamburger  = document.querySelector('.fa-hamburger');
+
+ const hiddenUL = document.querySelector("ul");
+
+ console.log(hamburger, hiddenUL);
+
+ hamburger.addEventListener('click', function(){
+  hiddenUL.classList.toggle("is-hidden--mobile");
+ })
