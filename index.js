193a4b7664557648af07542f7122ae05252d8194
Marvin <Player1@Marvins-World.local>
2019-09-30 20:03:52 -0500

    Add state.home.heading.

--- index.js
+++ index.js
@@ -1,6 +1,18 @@
 // Object Destructuring
 import {Header, Nav, Main, Footer} from "./components";
 
+//Use 'state' to render the appropriate heading depending on the state of the app
+
+//What page is currently selected or being displayed.
+const state = {
+  Home : {
+    heading: 'Home Page'
+  },
+  About : {
+    heading: 'About Page'
+  }
+}
+
 
 //import Header from "./components/Header";
 //import Nav from "./components/Nav";
@@ -10,7 +22,7 @@ import {Header, Nav, Main, Footer} from "./components";
 //currently,
 
 document.querySelector("#root").innerHTML = `
-${Header()}
+${Header(state.Home.heading)}
 ${Nav()}
 ${Main()}
 ${Footer()}
