635a980adc7b9473bfa9a04b4e87b432f82bcea7
Marvin <Player1@Marvins-World.local>
2019-10-21 21:29:26 -0500

    Add Admin to store.

--- store/index.js
+++ store/index.js
@@ -4,4 +4,4 @@ export {default as Contact} from './Contact';
 export {default as Gallery} from './Gallery';
 export {default as Blog} from './Blog';
 export {default as Links} from './Links';
-
+export {default as Admin} from "./Admin";
